6fefd52100d9216b2e4e23aaa0ad4d54678d58cd
Foundchair Done <dfc2333@outlook.com>
2026-01-11 10:30:35 +0800

    添加了很多说明，删除了很多个人信息
    如题

--- config.py
+++ config.py
@@ -35,10 +35,11 @@ qq_cookie_set_url = "https://jennergray.com:3301/user/setCookie?data="
 bili_avid_api_url = "https://api.bilibili.com/x/web-interface/wbi/search/all/v2"
 bili_cid_api_url = "https://api.bilibili.com/x/web-interface/view?"
 bili_video_api_url = "https://api.bilibili.com/x/player/wbi/playurl?"
-bili_cookie = decoder(b"4cMqtCFjRXgZaFACXRapCQ6gnHG81kBs4pMBhkLBkLsMWvSZd4fJH6s3ZmRSsWWueWz3HLzXxpsDcV7pHBeo3LWufwiMxcWWFSi7hcBtXm1f86j8dd9Btu64Zvw5qy76Nf8QLKTToz38BW7cYxU7qbkgZtXXyJSqZrhZHwrgCYyXA1AxqKVmK3ZQxKSRSkmSxKAbBQnmssVtpJzF6EGWAaDw1yWy5XSgs39qpdzs7J5Qx1McDLVfBXWCCC5jbJNHYEwU5a7VfCxfQfQVhGFPyZQb6SDAozoL3cFdowdavmgYRt4jGcpUgcFDk3AWVZZUGu8zrYLJBnVaFE2ShupMzobpPqQU162kqBgtZ76qNsXpwdAJZtv55C8oZiJQuW4wNWVcxvW6XhdVCczECgXsArfmqTVpy6y5NfUbsSZFJnJgCdQNT7HwRW2d5n7sQyc7ynkyB9hk2Yv3trKFKHwHafeiz3bXKPw9acZk6u8DQwhDcpqknsYQK3CxwjBsPVmbJ6KqQBU8aBP3BFFEKeNCiQohG2vvjrohssXrYGfAp2BLv74JSxrb89vNBRXSJopuCvmCtJKgy5n32MVTaoCSxpAxZtgDvTrZpDfKkCafLUN5yJnJEhT67uESc8dP9X8w5iKxbD2ASMi5U8WY57BSE9oz3Dh5gcvPU9tGxNqkLXTQEwpuYXDtnS65fXmmYbXt6yB74XUrVLCEx3sW1Uh8D4cS8wtauDSQJ4dCcYpjwNF85nBCQWvPHD177md8c4vkNqdtU3oE9xCDJtkBv1C1Qt9bo2fgWbBkXUF5FQD1rwcCqf7LvEMf8xjdrLmcJTEHHe279jExxeSGr9nGCJZHSDiCqvnidZXgsWyckd55AM1Zn43St3S3HcJYdULLRFvCG6yxCEzrecSmwntwuzsK6VawLhuwTJP5kxnbHNj4GtAPBmGNTUWSXEX2BE6DWznziqrtEcwxgUejio7GDebtkfvE5HpuTbeP6rUkdFSKZJuZjqGJ8Xj2G9CyBhGGCxQQBKPyFR21s1TXdeEcEBHAy8k2yVbh23DvxRBg3zAAe47s3i7MgjZKY5A3ei7UPxW7X5piZStf77sNAcH51G62aJGZSJGHoN6VMazRfWv5FcFToixQ5s4PzN73qwbHaLA2noiNqpcV4ytusHu9E3zJBJPgNDREEBc63Pyx34PoRNaHdZNQ34Cnd2yHtZ8KSNx5JsZyJPuBhLi6Cp1pJsXgNaAUJj5mSnGQo5Qeb7EYW4cACJP8HzB9x7vmVyDMnEQNccAkBGiyJ4euMyoA9TTW8FcPJS8oWfG6nHaR5FUpn9G3X5rJazXf1AUDLpGzW2zZMVeSdhSkjwHAMjnh38zZwPCYLouUijbYYC3L8tdxyaFyqh3KLoS6vWCRBsQeRxPF9a91PbEdD3F4hWHKqBnHnCrFQjGbZkcWUfZK1AtYgUczDupYXApm4w4NGTH1CiiLAfnFcK8pqVXsJW3XNVDiWBEGxWb8w3RK4DJcwTUJ9DwWxA3M5NkK4X85dEbtsjWVfdSdZDfYP8Smn73FDebccWEAPWjx73D7r45EY3ned7Mu7X3HAQioqPijxZDPt14fy")
+bili_cookie = decoder(b"<YOUR_ENCODED_COOKIE_HERE>")
 bili_headers = { "User-Agent": defaultUA, "Cookie": bili_cookie }
 #qq_cookie = requests.get("https://intellqc.com/user/getCookie?id="+decoder(b"hmdb4jwHChAP"), headers=headers, verify=False).json()["data"]["cookie"]
 qq_cookie=''
+deepseek_api_key_encoded = b'<YOUR_ENCODED_API_KEY_HERE>'
 
 # Directory Setup
 
@@ -50,15 +51,18 @@ net_dir = os.path.join(down_dir, "net")
 pages_dir = os.path.join(res_dir, "WebPages")
 log_dir = os.path.join(root, "logs")
 message_dir = os.path.join(root, "messages")
-for i in [message_dir, log_dir, pages_dir, loc_dir, net_dir, os.path.join(net_dir, "wyy"), os.path.join(net_dir, "qq"), os.path.join(net_dir, "bili")]:
+wyy_dir = os.path.join(net_dir,"wyy")
+qq_dir = os.path.join(net_dir,"qq")
+bili_dir = os.path.join(net_dir,"bili")
+for i in [bili_dir, qq_dir, wyy_dir, message_dir, log_dir, pages_dir, loc_dir, net_dir, down_dir]:
     if not os.path.exists(i):
         os.makedirs(i, exist_ok=True)
-with open(log_dir+'\\local.log','w') as llog:
+with open(os.path.join(log_dir,'local.log'),'w') as llog:
     llog.write('service started at {}<br/>\n'.format(str(datetime.datetime.now())))
 
 #Authentication Setup
 
-passwordEncoded=b'Ho1JuQQ6Jq7'
+passwordEncoded=b'<YOUR_ENCODED_PASSWORD_HERE>'
 
 if not os.path.exists(os.path.join(root, "allowed_ips.txt")):
     with open(os.path.join(root, "allowed_ips.txt"), "w") as f:
---
+++ encode.py
@@ -0,0 +1,22 @@
+def base58_encode(bytes):
+    """
+    将字节数组进行Base58编码
+    """
+    # 定义Base58字符集
+    chars = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+    
+    # 初始化编码后的字符串
+    encoded = ''
+    
+    # 计算字节数组的总数值
+    int_val = int.from_bytes(bytes, 'big')
+    
+    # 进行Base58编码
+    while int_val:
+        int_val, idx = divmod(int_val, 58)
+        encoded = chars[idx] + encoded
+    
+    return encoded
+data = b'YOUR_DATA_HERE'
+encoded = base58_encode(data)
+print(encoded)
